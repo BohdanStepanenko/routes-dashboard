6b7153be7e8e84435696499bf4b04695e363e182
BohdanStepanenko <stepanenko1905@gmail.com>
2023-10-27 00:52:09 +0300

    update routing

--- packages/bstepanenko/routes-dashboard/src/routes/web.php
+++ packages/bstepanenko/routes-dashboard/src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Bstepanenko\RoutesDashboard\RoutesDashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/routes-dashboard', function() {
-    return 'Routes Dashboard';
+Route::prefix('routes-dashboard')
+    ->name('routes-dashboard.')
+    ->group(function () {
+        Route::get('/', [RoutesDashboardController::class, 'renderDashboardPage'])->name('dashboard');
 });
