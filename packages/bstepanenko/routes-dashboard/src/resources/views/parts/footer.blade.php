305f763a58ec75b51d54298cc5e899f074f16a75
BohdanStepanenko <stepanenko1905@gmail.com>
2023-11-02 10:48:22 +0200

    frontend:include bootstrap

--- packages/bstepanenko/routes-dashboard/src/resources/views/parts/footer.blade.php
+++ packages/bstepanenko/routes-dashboard/src/resources/views/parts/footer.blade.php
@@ -4,6 +4,7 @@
         type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="{{ asset('vendor/routes-dashboard/js/charts.js') }}"></script>
 </body>
 </html>
--- packages/bstepanenko/routes-dashboard/src/resources/views/parts/header.blade.php
+++ packages/bstepanenko/routes-dashboard/src/resources/views/parts/header.blade.php
@@ -5,6 +5,7 @@
     <title>Routes Dashboard</title>
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <link rel="stylesheet" href="{{ asset('vendor/routes-dashboard/css/style.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous"/>
 </head>
