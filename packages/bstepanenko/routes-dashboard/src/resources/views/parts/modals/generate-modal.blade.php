41971941901ba3d2cbddc743793e5af70cc5b0bf
BohdanStepanenko <stepanenko1905@gmail.com>
2024-01-06 21:26:25 +0200

    frontend:implement export form

--- packages/bstepanenko/routes-dashboard/src/resources/views/parts/modals/generate-modal.blade.php
+++ packages/bstepanenko/routes-dashboard/src/resources/views/parts/modals/generate-modal.blade.php
@@ -9,30 +9,32 @@
             </div>
             <div class="modal-body">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="toggle-switch">
-                                <input type="checkbox" class="toggle-input" id="includeViewCheck">
-                                <label class="toggle-label" for="includeViewCheck"></label>
-                                <span class="form-check-label">Include View Routes</span>
+                    <form action="{{ route('routes-dashboard.export') }}" method="GET" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="toggle-switch">
+                                    <input type="checkbox" name="isView" class="toggle-input" id="includeViewCheck">
+                                    <label class="toggle-label" for="includeViewCheck"></label>
+                                    <span class="form-check-label">Include View Routes</span>
+                                </div>
+                                <div class="toggle-switch">
+                                    <input type="checkbox" name="generate" class="toggle-input" id="generateNewFileCheck">
+                                    <label class="toggle-label" for="generateNewFileCheck"></label>
+                                    <span class="form-check-label">Generate New Route File</span>
+                                </div>
+                                <div class="toggle-switch">
+                                    <input type="checkbox" class="toggle-input" id="sourceCheck">
+                                    <label class="toggle-label" for="sourceCheck"></label>
+                                    <span class="form-check-label">Show Source</span>
+                                </div>
                             </div>
-                            <div class="toggle-switch">
-                                <input type="checkbox" class="toggle-input" id="generateNewFileCheck">
-                                <label class="toggle-label" for="generateNewFileCheck"></label>
-                                <span class="form-check-label">Generate New Route File</span>
-                            </div>
-                            <div class="toggle-switch">
-                                <input type="checkbox" class="toggle-input" id="sourceCheck">
-                                <label class="toggle-label" for="sourceCheck"></label>
-                                <span class="form-check-label">Show Source</span>
+                            <div class="col-4 text-center">
+                                <button type="submit" name="export_type" value="all_routes" class="export-btn">Export All Routes</button>
+                                <button type="submit" name="export_type" value="other_routes" class="export-btn">Export Definitions</button>
+                                <button type="button" class="postman-btn">Export Postman Collection</button>
                             </div>
                         </div>
-                        <div class="col-4 text-center">
-                            <button type="button" class="export-btn">Export All Routes</button>
-                            <button type="button" class="export-btn">Export Definitions</button>
-                            <button type="button" class="postman-btn">Export Postman Collection</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
                 <div id="generatedSource" class="code-container w-hidden">
                     <pre><code id="code">{{ trim($generatedApiFileContent) }}</code></pre>
